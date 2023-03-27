7af9711a2deab0c80932078c4fcdc6b7438f2e20
Alymbek <you@example.com>
2023-03-27 16:45:10 +0600

    21: Сделать перевод системы хранимым

--- src/modules/app/app.component.ts
+++ src/modules/app/app.component.ts
@@ -3,8 +3,10 @@ import { ActivationEnd, NavigationEnd, Router, Routes } from '@angular/router';
 import { delay, Subject, Subscription, switchMap } from 'rxjs';
 import { ApiService } from '../api/api.service';
 import { IUser } from '../api/models/user.model';
-import { TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { Title } from '@angular/platform-browser';
+import { StoreService } from '../api/store.service';
+import { ELanguage, ILanguage } from '../api/models/language.model';
 
 @Component({
   selector: 'app-root',
@@ -19,9 +21,14 @@ export class AppComponent implements OnInit, OnDestroy {
   pageTitle$ = new Subject<void>();
   subscriptions: Subscription[] = [
     this.router.events.subscribe((e) => this.handleRouterEvent(e)),
-    this.translate.onLangChange.pipe(switchMap(() => this.translate.get(this.currentPageTitle)))
-      .subscribe(res => this.titleService.setTitle(res)),
-    this.pageTitle$.pipe(switchMap(() => this.translate.get(this.currentPageTitle)), delay(1))
+    this.translate.onLangChange.subscribe((langObj) =>
+      this.handleLangChange(langObj)
+    ),
+    this.pageTitle$
+      .pipe(
+        switchMap(() => this.translate.get(this.currentPageTitle)),
+        delay(1)
+      )
       .subscribe((tr: string) => this.titleService.setTitle(tr)),
   ];
 
@@ -30,16 +37,51 @@ export class AppComponent implements OnInit, OnDestroy {
     private router: Router,
     private translate: TranslateService,
     private titleService: Title,
-  ) {
+    private store: StoreService
+  ) {}
+
+  private initLang(): void {
+    const languageStore = this.store.getItem<ILanguage>('language');
+
+    if (languageStore != null) {
+      this.translate.addLangs(languageStore.all);
+      this.translate.setDefaultLang(languageStore.default);
+      this.translate.use(languageStore.current);
+    } else {
+      const languageConf: ILanguage = {
+        default: ELanguage.ru,
+        current: ELanguage.ru,
+        all: [ELanguage.en, ELanguage.ru, ELanguage.ky],
+      };
+      this.store.setItem<ILanguage>('language', languageConf);
+      this.translate.addLangs(languageConf.all);
+      this.translate.setDefaultLang(languageConf.default);
+      this.translate.use(languageConf.current);
+    }
+  }
+
+  private updateStoreOnChangeLang(current: ELanguage): void {
+    const languageStore = this.store.getItem<ILanguage>('language');
+    languageStore.current = current;
+    this.store.setItem<ILanguage>('language', languageStore);
   }
 
   ngOnInit(): void {
+    this.initLang();
     this.currentUser = this.api.user.getLoggedInUser();
     this.routes = this.createRoutes(this.router.config);
   }
 
   ngOnDestroy(): void {
     this.subscriptions.forEach((s) => s.unsubscribe());
+  }
+
+  handleLangChange(langObj: LangChangeEvent): void {
+    this.updateStoreOnChangeLang(langObj.lang as ELanguage);
+    const trTitle = langObj.translations[this.currentPageTitle];
+    if (trTitle != null) {
+      this.titleService.setTitle(trTitle);
+    }
   }
 
   handleRouterEvent(event: unknown): void {
