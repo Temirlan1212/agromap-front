b9053beb784ea82e1059ab854aabb467e3f80202
Sanzhar <snzhrski@gmail.com>
2023-04-04 13:33:31 +0600

    agro-49: Профиль

--- src/modules/app/modules/profile/profile.component.ts
+++ src/modules/app/modules/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/modules/api/api.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -8,6 +9,12 @@ import { ApiService } from 'src/modules/api/api.service';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
+  form: FormGroup = new FormGroup({
+    fullname: new FormControl<string | null>(null),
+    phone: new FormControl<string | null>(null),
+    password: new FormControl<string | null>(null),
+  });
+
   constructor(private api: ApiService, private router: Router) {}
 
   handleLogoutClick(): void {
