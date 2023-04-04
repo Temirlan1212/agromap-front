9ceac3dbb7a264c9505c1a2d466611797b58e257
Sanzhar <snzhrski@gmail.com>
2023-04-04 14:48:41 +0600

    agro-49: Профиль

--- src/modules/app/modules/profile/profile.component.ts
+++ src/modules/app/modules/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/modules/api/api.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MessagesService } from '../../../ui/components/services/messages.service';
 
 @Component({
   selector: 'app-profile',
@@ -15,10 +16,21 @@ export class ProfileComponent {
     password: new FormControl<string | null>(null),
   });
 
-  constructor(private api: ApiService, private router: Router) {}
+  constructor(
+    private api: ApiService,
+    private router: Router,
+    private messages: MessagesService
+  ) {}
 
   handleLogoutClick(): void {
     this.api.user.logOut();
     this.router.navigate(['']);
+  }
+
+  async handleSaveClick(): Promise<void> {
+    try {
+    } catch (e: any) {
+      this.messages.error(e.message);
+    }
   }
 }
--- src/modules/app/modules/profile/profile.module.ts
+++ src/modules/app/modules/profile/profile.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from '../../../ui/components/input/input.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [
-    ProfileComponent
-  ],
+  declarations: [ProfileComponent],
   imports: [
     CommonModule,
-    ProfileRoutingModule
-  ]
+    ProfileRoutingModule,
+    ReactiveFormsModule,
+    InputComponent,
+    TranslateModule,
+  ],
 })
-export class ProfileModule { }
+export class ProfileModule {}
