02a9627a662d2aa8f79f84c7b2707024df07378e
Master <isa.smail1123@gmail.com>
2023-04-19 11:17:58 +0600

    Создать компонент input-slider

--- src/modules/ui/components/input-range/input-range.component.ts
+++ src/modules/ui/components/input-range/input-range.component.ts
@@ -2,30 +2,32 @@ import { Component, Input, OnInit, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
-  selector: 'app-input-slider',
-  templateUrl: './input-slider.component.html',
-  styleUrls: ['./input-slider.component.scss'],
+  selector: 'app-input-range',
+  templateUrl: './input-range.component.html',
+  styleUrls: ['./input-range.component.scss'],
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => InputSliderComponent),
+      useExisting: forwardRef(() => InputRangeComponent),
       multi: true,
     },
   ],
   standalone: true,
 })
-export class InputSliderComponent implements ControlValueAccessor, OnInit {
+export class InputRangeComponent implements ControlValueAccessor, OnInit {
   @Input() value: number = 1;
   @Input() disabled: boolean = false;
   @Input() max: number = 50;
   @Input() min: number = 0;
   @Input() step: number = 1;
+  @Input() id: number | string = 1;
+  @Input() name: string = 'name';
   @Input() height: number = 0.1;
 
   onChange: Function = () => null;
   onTouched: Function = () => null;
 
-  onSliderChange(e: Event): void {
+  handleChange(e: Event): void {
     const value = (e.target as any).value;
     this.value = value;
     this.onChange(value);
