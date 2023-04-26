8c6d55e37ef7d41a3721f747633fc3013d7f0233
Alymbek <you@example.com>
2023-04-26 11:07:47 +0600

    fix: 26.04.2023 11:07

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   url: 'http://localhost:4200',
   apiUrl: 'http://10.118.50.27:8111',
-  geoserverUrl: 'http://geoserver.24mycrm.com',
+  geoserverUrl: 'https://geoserver.24mycrm.com',
 };
