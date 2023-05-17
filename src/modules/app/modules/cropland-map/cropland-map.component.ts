6d491ea80b627e65efa3677d5af8eac5af7526d9
Master <isa.smail1123@gmail.com>
2023-05-17 16:54:13 +0600

    1) Добавить id контура в подробнее одного контура. 2) Баг при изминении основного слоя и ии в cropland-map иногда не меняется

--- src/modules/app/modules/cropland-map/cropland-map.component.ts
+++ src/modules/app/modules/cropland-map/cropland-map.component.ts
@@ -543,18 +543,35 @@ export class CroplandMapComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   handleModeChange(mode: string | null) {
-    this.mode = mode as string;
+    if (this.mode === mode) {
+      this.mode =
+        mode === 'agromap_store_ai' ? 'agromap_store' : 'agromap_store_ai';
+    } else {
+      this.mode = mode as string;
+    }
   }
 
   handleWmsLayerChanged(layer: ITileLayer | null): void {
     this.mapData?.geoJson.clearLayers();
     this.getRegionsPolygon();
+
     if (layer != null) {
       const finded = this.wmsLayers.find((l) => l.name === layer.name);
       if (finded != null && finded.name === 'agromap_store_ai') {
         this.isWmsAiActive = true;
       } else {
         this.isWmsAiActive = false;
+      }
+    }
+
+    if (this.mapData?.map) {
+      const mapBounds = this.store.getItem<Record<string, LatLngBounds>>(
+        'ArableLandComponent'
+      )?.['mapBounds'];
+
+      if (mapBounds) {
+        this.addPolygonsInScreenToMap(mapBounds);
+        this.cd.detectChanges();
       }
     }
   }
