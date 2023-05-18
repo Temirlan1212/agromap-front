3736bbb6857b29269b43a873981dea5c7aa88f77
Master <isa.smail1123@gmail.com>
2023-05-18 11:08:04 +0600

    Создать свой storage для seletectedFeature в pastures-map

--- src/modules/app/modules/cropland-map/components/contour-filter/contour-filter.component.ts
+++ src/modules/app/modules/cropland-map/components/contour-filter/contour-filter.component.ts
@@ -108,7 +108,6 @@ export class ContourFilterComponent implements OnInit, OnDestroy {
       this.mapGeo = res?.geoJson as GeoJSON;
     }),
     this.store.watchItem('MapControlLayersSwitchComponent').subscribe((v) => {
-      this.onModeChanged.emit(v.filterControlLayerSwitch?.name);
       this.mode?.patchValue(v.filterControlLayerSwitch?.oldValue, {
         emitEvent: false,
       });
