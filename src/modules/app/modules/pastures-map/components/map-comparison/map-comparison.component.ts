ffb2707595d4c0954d3084907b360147fadc09a3
Master <isa.smail1123@gmail.com>
2023-05-26 10:30:33 +0600

    Добавить разделение экрана с возможностью сравнивать уражайность

--- src/modules/app/modules/pastures-map/components/map-comparison/map-comparison.component.ts
+++ src/modules/app/modules/pastures-map/components/map-comparison/map-comparison.component.ts
@@ -31,8 +31,7 @@ export class MapComparisonComponent
 {
   @ViewChildren(YieldMapComponent)
   yieldMapComponents!: QueryList<YieldMapComponent>;
-  loading: boolean = false;
-
+  loading = false;
   subscriptions: Subscription[] = [];
   maps: ILeafletMap[] = [];
   polygons!: GeoJSON;
@@ -44,30 +43,32 @@ export class MapComparisonComponent
   constructor(private store: StoreService) {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['filterFormResetValues']) {
-      this.filterFormValues = changes['filterFormResetValues'];
-      if (this.yieldMapComponents) {
-        this.yieldMapComponents.first.handleFilterFormReset(
-          this.filterFormValues.currentValue
-        );
-      }
+    if ('filterFormResetValues' in changes) {
+      this.filterFormValues = changes['filterFormResetValues'].currentValue;
+      this.handleFilterFormReset();
     }
 
-    if (changes['filterFormValues']) {
-      this.filterFormValues = changes['filterFormValues'];
-      if (this.yieldMapComponents) {
-        this.yieldMapComponents.first.handleFilterFormSubmit(
-          this.filterFormValues.currentValue
-        );
-      }
+    if ('filterFormValues' in changes) {
+      this.filterFormValues = changes['filterFormValues'].currentValue;
+      this.handleFilterFormSubmit();
     }
   }
 
-  syncMaps(maps: ILeafletMap[]) {
-    for (const key1 in maps) {
-      for (const key2 in maps) {
-        if (key1 !== key2 && maps[key1] && maps[key2]) {
-          (maps[key1] as ILeafletMap).sync(maps[key2], {
+  handleFilterFormReset(): void {
+    this.yieldMapComponents?.first.handleFilterFormReset(this.filterFormValues);
+  }
+
+  handleFilterFormSubmit(): void {
+    this.yieldMapComponents?.first.handleFilterFormSubmit(
+      this.filterFormValues
+    );
+  }
+
+  syncMaps(maps: ILeafletMap[]): void {
+    maps.forEach((map1, index1) => {
+      maps.forEach((map2, index2) => {
+        if (index1 !== index2 && map1 && map2) {
+          map1.sync(map2, {
             syncCursor: true,
             syncCursorMarkerOptions: {
               radius: 5,
@@ -77,93 +78,87 @@ export class MapComparisonComponent
             },
           });
         }
-      }
-    }
+      });
+    });
   }
 
-  onEachFeature = (
+  onEachFeature(
     mapLayerFeature: MapLayerFeature,
     component: YieldMapComponent
-  ) => {
+  ): void {
     component.handleFeatureClick(mapLayerFeature);
     component.mapComponent.featureOpen = true;
-  };
+  }
 
-  featureClose = (component: YieldMapComponent) => {
+  featureClose(component: YieldMapComponent): void {
     component.mapComponent.featureOpen = false;
-
     component.handleFeatureClose();
-  };
+  }
 
-  ngAfterViewInit(): void {
-    this.yieldMapComponents.first.handleFilterFormSubmit(
-      this.filterFormValues.currentValue
-    );
-
-    const refs = this.yieldMapComponents;
-    this.maps = refs.map((ref) => ref.mapData?.map as ILeafletMap);
-
-    this.syncMaps(this.maps);
-
-    const firstYieldMapComponents = this.yieldMapComponents.first;
-
-    const mapMoveSub = firstYieldMapComponents.mapComponent.mapMove.subscribe(
-      async (mapMove: MapMove) => {
-        this.store.setItem<Record<string, LatLngBounds>>('HomeComponent', {
-          mapBounds: mapMove.bounds,
-        });
-        if (mapMove.zoom > 13) {
-          this.loading = true;
-          this.polygons = await firstYieldMapComponents.getPolygonsInScreen(
-            mapMove.bounds
-          );
-          this.loading = false;
-        }
-
-        refs.map((ref) => {
-          if (ref.mapData?.map != null) {
-            if (mapMove.zoom > 13) {
-              ref.mapData.geoJson.clearLayers();
-              ref.addPolygonsInScreenToMap(this.polygons);
-              ref.getRegionsPolygon();
-            }
-
-            if (mapMove.zoom < 12) ref.activeContourSmall = null;
-          }
-        });
-      }
-    );
-
-    const subscriptions = refs.map((ref) => {
-      const featureCloseSub = ref.mapComponent.featureClose.subscribe(() =>
-        refs.forEach((ref) => this.featureClose(ref))
-      );
-
-      const featureClickSub = ref.mapComponent.featureClick.subscribe(
-        (mapLayerFeature: MapLayerFeature) =>
-          refs.forEach((ref) => this.onEachFeature(mapLayerFeature, ref))
-      );
-
-      const featureMouseOverSub = ref.mapComponent.featureHover.subscribe(
-        (layerFeature: MapLayerFeature) =>
-          refs.forEach((ref) => ref.handleFeatureMouseOver(layerFeature))
-      );
-
-      const featureMouseLeaveSub = ref.mapComponent.featureUnhover.subscribe(
-        (layerFeature: MapLayerFeature) =>
-          refs.forEach((ref) => ref.handleFeatureMouseLeave(layerFeature))
-      );
-
-      return [
-        featureCloseSub,
-        featureClickSub,
-        featureMouseOverSub,
-        featureMouseLeaveSub,
-      ];
+  async handleMapMove(mapMove: MapMove): Promise<void> {
+    this.store.setItem<Record<string, LatLngBounds>>('HomeComponent', {
+      mapBounds: mapMove.bounds,
     });
 
-    this.subscriptions = subscriptions.flat();
-    this.subscriptions.push(mapMoveSub);
+    if (mapMove.zoom > 13) {
+      this.loading = true;
+      this.polygons = await this.yieldMapComponents.first.getPolygonsInScreen(
+        mapMove.bounds
+      );
+      this.loading = false;
+    }
+
+    this.yieldMapComponents.forEach((ref) => {
+      const mapData = ref.mapData;
+      if (mapData?.map != null) {
+        if (mapMove.zoom > 13) {
+          mapData.geoJson.clearLayers();
+          ref.addPolygonsInScreenToMap(this.polygons);
+          ref.getRegionsPolygon();
+        }
+
+        if (mapMove.zoom < 12) {
+          ref.activeContourSmall = null;
+        }
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.handleFilterFormSubmit();
+    this.maps = this.yieldMapComponents.map(
+      (ref) => ref.mapData?.map as ILeafletMap
+    );
+    this.syncMaps(this.maps);
+
+    const refs = this.yieldMapComponents;
+
+    refs.forEach((ref) => {
+      const mapComponent = ref.mapComponent;
+      const subscriptions = [
+        mapComponent.featureClose.subscribe(() =>
+          refs.forEach((ref) => this.featureClose(ref))
+        ),
+        mapComponent.featureClick.subscribe(
+          (mapLayerFeature: MapLayerFeature) =>
+            refs.forEach((ref) => this.onEachFeature(mapLayerFeature, ref))
+        ),
+        mapComponent.featureHover.subscribe((layerFeature: MapLayerFeature) =>
+          refs.forEach((ref) => ref.handleFeatureMouseOver(layerFeature))
+        ),
+        mapComponent.featureUnhover.subscribe((layerFeature: MapLayerFeature) =>
+          refs.forEach((ref) => ref.handleFeatureMouseLeave(layerFeature))
+        ),
+      ];
+
+      this.subscriptions.push(...subscriptions);
+    });
+
+    this.subscriptions.push(
+      refs.first.mapComponent.mapMove.subscribe(async (mapMove: MapMove) =>
+        this.handleMapMove(mapMove)
+      )
+    );
   }
 
   ngOnDestroy(): void {
