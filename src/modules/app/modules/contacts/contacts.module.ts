e0fc94b5a55e8e15342ff27242d57f919e62eb7f
Tima1212 <ltemirlan2003@gmail.com>
2023-06-29 14:45:17 +0600

    добавить компонент contact-information

--- src/modules/app/modules/contacts/contacts.module.ts
+++ src/modules/app/modules/contacts/contacts.module.ts
@@ -6,10 +6,9 @@ import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { SidePanelComponent } from 'src/modules/ui/components/side-panel/side-panel.component';
 import { SvgIconComponent } from 'src/modules/ui/components/svg-icon/svg-icon.component';
-import { ContactInformationComponent } from './components/contact-information/contact-information.component';
 
 @NgModule({
-  declarations: [ContactsComponent, ContactInformationComponent],
+  declarations: [ContactsComponent],
   imports: [
     CommonModule,
     ContactsRoutingModule,
