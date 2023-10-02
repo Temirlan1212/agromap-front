23f52ae9d2491b38f8173a91ccde34444d4534e8
Temirlan Kadyrov <ltemirlan2003@gmail.com>
2023-10-02 09:36:52 +0600

    Добавить слой zemdoli

--- src/modules/app/modules/pastures-map/pastures-map.component.ts
+++ src/modules/app/modules/pastures-map/pastures-map.component.ts
@@ -268,6 +268,15 @@ export class PasturesMapComponent implements OnInit, OnDestroy, AfterViewInit {
       }),
       type: 'checkbox',
     },
+    {
+      title: 'Zemdoli',
+      name: 'agromap:zemdoli',
+      layer: tileLayer.wms('https://geoserver.24mycrm.com/agromap/wms', {
+        layers: 'agromap:zemdoli',
+        ...this.wmsLayersOverlayOptions,
+      }),
+      type: 'checkbox',
+    },
   ];
 
   constructor(
