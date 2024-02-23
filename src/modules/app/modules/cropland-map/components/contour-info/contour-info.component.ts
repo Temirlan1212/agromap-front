9d4d95e98352eb9c095d2391987c4d77d990bbec
Mac <mac@abs-MacBook-Pro.local>
2024-02-23 16:47:57 +0600

    add: contour-info

--- src/modules/app/modules/cropland-map/components/contour-info/contour-info.component.ts
+++ src/modules/app/modules/cropland-map/components/contour-info/contour-info.component.ts
@@ -1,47 +1,46 @@
-import {
-  Component,
-  Input,
-  OnChanges,
-  OnDestroy,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { ApiService } from '../../../../../api/api.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
+import { CroplandMainLayerService } from '../../lib/services/layer.service';
 
 @Component({
   selector: 'app-contour-info',
   templateUrl: './contour-info.component.html',
   styleUrls: ['./contour-info.component.scss'],
 })
-export class ContourInfoComponent implements OnChanges, OnDestroy {
-  @Input() contourId!: number;
+export class ContourInfoComponent implements OnDestroy {
   @Input() isWmsAiActive!: boolean;
   loading: boolean = false;
-  contour!: any;
+  activeContour!: any;
   currentLang: string = this.translateSvc.currentLang;
-  sub: Subscription = this.translateSvc.onLangChange.subscribe(
-    (res) => (this.currentLang = res.lang)
-  );
+  hidden: boolean = false;
+  subs: Subscription[] = [];
 
   constructor(
     private api: ApiService,
-    private translateSvc: TranslateService
-  ) {}
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['contourId']) {
-      this.getContour();
-    }
+    private translateSvc: TranslateService,
+    private layerService: CroplandMainLayerService
+  ) {
+    const sub1 = this.translateSvc.onLangChange.subscribe(
+      (res) => (this.currentLang = res.lang)
+    );
+    const sub2 = this.layerService.selectProperties.subscribe(async (v) => {
+      if (!!v['id']) {
+        // await this.getContour(v.id);
+        // this.hidden = !v.id;
+      }
+    });
+    this.subs.push(...[sub1, sub2]);
   }
 
-  async getContour(): Promise<void> {
+  async getContour(contourId: number): Promise<void> {
     this.loading = true;
     try {
       if (this.isWmsAiActive) {
-        this.contour = await this.api.aiContour.getOne(this.contourId);
+        this.activeContour = await this.api.aiContour.getOne(contourId);
       } else {
-        this.contour = await this.api.contour.getOne(this.contourId);
+        this.activeContour = await this.api.contour.getOne(contourId);
       }
     } catch (e) {
       console.log(e);
@@ -50,7 +49,9 @@ export class ContourInfoComponent implements OnChanges, OnDestroy {
     }
   }
 
+  handleCancelClick() {}
+
   ngOnDestroy() {
-    this.sub.unsubscribe();
+    this.subs.map((sub) => sub.unsubscribe());
   }
 }
